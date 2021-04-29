84b2f2f3dbcbed2eae22ca57ac48a97bf885c259
OsmiumOP <71386574+OsmiumOP@users.noreply.github.com>
2021-04-29 12:32:32 +0530

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,35 +1,48 @@
 QBCore = nil
 
-Citizen.CreateThread(function()
-    while QBCore == nil do
-        TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-        Citizen.Wait(0)
+Citizen.CreateThread(function() 
+    while true do
+        Citizen.Wait(10)
+        if QBCore == nil then
+            TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)    
+            Citizen.Wait(200)
+        end
     end
 end)
 
--- CONFIG VARS
-grannypos = { x = 3312.95, y = 5178.88, z = 18.63, h = 210.79 }
-model = "ig_mrs_thornhill"
+isLoggedIn = true
+
+RegisterNetEvent("QBCore:Client:OnPlayerUnload")
+AddEventHandler("QBCore:Client:OnPlayerUnload", function()
+    isLoggedIn = false
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    isLoggedIn = true
+    SpawnPed()
+end)
+
+grannypos = { x = 1596.92, y = 3596.03, z = 37.8, h = 114.99 }
+model = "cs_orleans"
 
 
-    Citizen.CreateThread(function()
-
-       local hash = GetHashKey(model) -- Store model as Hash
+function SpawnPed()
+if isLoggedIn then 
+       local hash = GetHashKey(model) 
        RequestModel(hash)
-        -- Wait until Model has loaded
+        
         while not HasModelLoaded(hash) do Citizen.Wait(0) end
+
         ped = CreatePed(4, hash, grannypos.x, grannypos.y, grannypos.z, grannypos.h, true, true)
 
-        -- Makes the ped not run away/die/get hurt/etc
+        
         FreezeEntityPosition(ped, true)
         SetEntityInvincible(ped, true)
         SetBlockingOfNonTemporaryEvents(ped, true)
-
-        local pedanimdict = "mini@drinking"
-        local pedanim = "shots_barman_b"
-        loadAnimDict(pedanimdict)	
-        TaskPlayAnim(ped, pedanimdict, pedanim, 3.0, 3.0, -1, 49, 1, 0, 0, 0)
-    end)
+	end	
+end
+end
 
 function loadAnimDict(dict)
 	while (not HasAnimDictLoaded(dict)) do
@@ -52,36 +65,36 @@ Citizen.CreateThread(function()
         if distance < 10 then
             if not IsPedInAnyVehicle(PlayerPedId(), true) then
                 if distance < 3 then
-                    QBCore.Functions.DrawText3D(grannypos.x, grannypos.y, grannypos.z + 0.5, '[E] - Request Grandma for $2,000')
+                    QBCore.Functions.DrawText3D(grannypos.x, grannypos.y, grannypos.z + 0.5, '[E] - ﺮﺣﺎﺴﻟﺍ ﺓﺪﻋﺎﺴﻣ ﺐﻠﻃ $2,000')
                     DisableControlAction(0, 57, true)
                     if IsDisabledControlJustReleased(0, 54) then
 
-                        QBCore.Functions.Progressbar("check-", "Requesting Grandma for Help", 5000, false, true, {
+                        QBCore.Functions.Progressbar("check-", "طلب مساعدة الساحر", 5000, false, true, {
                             disableMovement = true,
                             disableCarMovement = true,
                             disableMouse = false,
                             disableCombat = true,
                         }, {}, {}, {}, function()
-                            if prob > 5 then
+                            if prob > 1 then
                              loadAnimDict('missheistdockssetup1clipboard@base')
                              TaskPlayAnim( PlayerPedId(), "missheistdockssetup1clipboard@base", "base", 3.0, 1.0, -1, 49, 0, 0, 0, 0 ) 
                              loadAnimDict(healAnimDict)	
                              TaskPlayAnim(ped, healAnimDict, healAnim, 3.0, 3.0, 8000, 49, 0, 0, 0, 0)
-                             QBCore.Functions.Progressbar("check-", "Blessing You with a Life", 10000, false, true, {
+                             QBCore.Functions.Progressbar("check-", "جاري الانعاش", 10000, false, true, {
                                 disableMovement = true,
                                 disableCarMovement = true,
                                 disableMouse = false,
                                 disableCombat = true,
                              }, {}, {}, {}, function()
-                                QBCore.Functions.Notify("You're were treated! ..", "success")
+                                QBCore.Functions.Notify("You're were treated! ..", "succes")
                                 TriggerEvent('hospital:client:Revive')
-                                TriggerServerEvent('osm-grandma:server:Charge')
+                                TriggerServerEvent('AbD-monkey:server:Charge')
                                 ClearPedTasks(PlayerPedId())
-                             end, function() -- Cancel
+                             end, function() 
                                 ClearPedTasks(PlayerPedId())
                             end)
                         else 
-                            QBCore.Functions.Notify("Grandma Refused to Help", "error")
+                            QBCore.Functions.Notify("Monkey Refused to Help", "error")
                         end 
                         end)
                             
